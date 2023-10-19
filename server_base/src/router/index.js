d1a55d5aab689edb511914e5472e7715cfbea239
Brando S.A <141217206+sirDuuck@users.noreply.github.com>
2023-10-19 16:35:20 -0300

    delete

--- server_base/src/router/index.js
+++ server_base/src/router/index.js
@@ -1,9 +1,23 @@
 import express from 'express';
-import { getFunction, postFunction, putFunction} from './server_base/src/controller/client';
-import { getFunction, postFunction, putFunction} from './server_base/src/controller/certificado';
+
+import { getFunction, postFunction, putFunction, deleteFuncion} from './server_base/src/controller/client';
+
+import { getFunction, postFunction, putFunction, deleteFuncion} from './server_base/src/controller/certificado';
 
 const rota = express.Router();
 
 rota.get('/user', (resquest, response) => response.status(200).send ('hello world'));
+
+rota.delete('./user/:id', (request, response) => {
+    const id = request.params.id; 
+
+    response.status(204).send();
+});
+
+rota.get('/user/:id', (request, response) => {
+    const id = request.params.id;
+
+    response.status(200).json({id,});
+});
 
 export default rota;
